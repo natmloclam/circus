107aa284b26dd9ba764d10118d9b5511e04f68ed
Malcolm Tan <malcolmtan2003@gmail.com>
2026-02-18 16:09:09 +0800

    Add method to print all animals

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -52,5 +52,10 @@ public class Circus {
         makeAnimalsTalk();
         System.out.println("Total value of animals " + calculateAssetValue(animals));
         System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+    private static void printAllAnimals(ArrayList<Animal> animalArrayList) {
+        for (Animal a : animalArrayList) {
+            System.out.println(a);
+        }
+    }
     }
 }
